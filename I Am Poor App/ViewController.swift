ed623c2f2b87aca7cc3f137553a32f65b68bf4f5
Gospodi <gospodijazzet@gmail.com>
2022-01-19 14:07:27 +0300

    Initial Commit

---
+++ I Am Poor App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor App
+//
+//  Created by Gospodi on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
